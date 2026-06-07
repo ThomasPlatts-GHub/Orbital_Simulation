64bbc7ef4f4f5ad0f23bf98c5e4d6d67024807ec
ThomasPlatts-GHub <thomasplatts2004@gmail.com>
2026-06-07 21:51:40 +0100

    Instantiation moved outside loop for mem purposes

--- Main/main.py
+++ Main/main.py
@@ -1,12 +1,17 @@
 import pygame
 import numpy as np
-from Main.C_planet import Planet
+from C_planet import Planet
 
 pygame.init()
 window = pygame.display.set_mode((1280,720))
 window_midpoint = (640, 360)
 clock = pygame.time.Clock()
 running = True
+
+# Planet instantiations
+mercury = Planet("Mercury", 5, (158,158,158), 100)
+venus = Planet("Venus", 10, (217,108,0), 150)
+earth = Planet("Earth", 15, (0,0,255), 225)
 
 while running:
     for event in pygame.event.get():
@@ -17,9 +22,6 @@ while running:
 
     # Draw star + planets
     pygame.draw.circle(window, (255,234,0), window_midpoint, 35) # Star
-    mercury = Planet("Mercury", 5, (158,158,158), 100)
-    venus = Planet("Venus", 10, (217,108,0), 150)
-    earth = Planet("Earth", 15, (0,0,255), 225)
     mercury.draw(window, window_midpoint, pygame.time.get_ticks()*0.001)
     venus.draw(window, window_midpoint, pygame.time.get_ticks()*0.001)
     earth.draw(window, window_midpoint, pygame.time.get_ticks()*0.001)
